7c73c998b025fc95923a42eb882369ef36e20631
Emma Dayton <emma.dayton85@gmail.com>
2019-03-11 23:18:39 -0700

    build out basic frame of html, add some line spacing on server

--- server.py
+++ server.py
@@ -7,10 +7,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/results', methods=['POST'])
-def results():
+@app.route('/add_pet', methods=['POST'])
+def add_pet():
     # submit info and SQL query go here
     return redirect('/')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
